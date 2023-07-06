35040485b5261af84c6c85d2b94dcc008acb5e8e
TianYuan <white-sky@qq.com>
2023-07-06 06:30:48 +0000

    add synthesize.sh which input prompt_semantic_path prompt_acoustic_path and target_semantic_path

--- egs_s2/LibriTTS/local/synthesize.sh
+++ egs_s2/LibriTTS/local/synthesize.sh
@@ -1,1 +1,21 @@
 #!/bin/bash
+# synthesize input 需要自己构造一下三个输入用于测试，最好把原始 wav 找到
+# 1. prompt_semantic
+# 2. prompt_acoustic
+# 3. target_semantic
+# 1 和 2 需要是同一个音频提取的
+
+config_path=$1
+train_output_path=$2
+ckpt_name=$3
+root_dir=$4
+
+python3 ${BIN_DIR}/synthesize.py \
+        --config_file=${config_path} \
+        --ckpt_path=${root_dir}/${train_output_path}/checkpoint/${ckpt_name} \
+        --prompt_semantic_path=${root_dir}/dump/test/synthesize_input/prompt_semantic.tsv \
+        --prompt_acoustic_path=${root_dir}/dump/test/synthesize_input/prompt_acoustic.pth \
+        --target_semantic_path=${root_dir}/dump/test/synthesize_input/target_semantic.tsv \
+        --output_dir=${root_dir}/${train_output_path}/test_output \
+        --hificodec_model_path=pretrained_model/hificodec/HiFi-Codec-16k-320d \
+        --hificodec_config_path=pretrained_model/hificodec/config_16k_320d.json
