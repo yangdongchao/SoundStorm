4d6034363ab1272c9dbedd448b55e2bfdb67d195
TianYuan <white-sky@qq.com>
2023-07-12 06:26:13 +0000

    add layer control for hubert_kmeans.py and semantic_tokenizer.py

--- soundstorm/s2/models/hubert/hubert_kmeans.py
+++ soundstorm/s2/models/hubert/hubert_kmeans.py
@@ -1,13 +1,14 @@
+import logging
 from pathlib import Path
 
 import fairseq
 import joblib
 import torch
-from einops import pack
-from einops import unpack
+from einops import rearrange
+from einops import repeat
 from torch import nn
 from torchaudio.functional import resample
-
+logging.root.setLevel(logging.ERROR)
 
 def curtail_to_multiple(t, mult):
     data_len = t.shape[-1]
@@ -18,15 +19,27 @@ def exists(val):
     return val is not None
 
 
+def default(val, d):
+    return val if exists(val) else d
+
+
 class HubertWithKmeans(nn.Module):
+    """
+    checkpoint and kmeans can be downloaded at https://github.com/facebookresearch/fairseq/tree/main/examples/hubert
+    or you can train your own
+    """
+
     def __init__(self,
                  checkpoint_path,
                  kmeans_path,
                  target_sample_hz=16000,
-                 seq_len_multiple_of=None):
+                 seq_len_multiple_of=None,
+                 output_layer=9):
         super().__init__()
+
         self.target_sample_hz = target_sample_hz
         self.seq_len_multiple_of = seq_len_multiple_of
+        self.output_layer = output_layer
 
         model_path = Path(checkpoint_path)
         kmeans_path = Path(kmeans_path)
@@ -43,7 +56,11 @@ class HubertWithKmeans(nn.Module):
         self.model.eval()
 
         kmeans = joblib.load(kmeans_path)
+
         self.kmeans = kmeans
+
+        self.register_buffer('cluster_centers',
+                             torch.from_numpy(kmeans.cluster_centers_))
 
     @property
     def groups(self):
@@ -53,9 +70,14 @@ class HubertWithKmeans(nn.Module):
     def codebook_size(self):
         return self.kmeans.n_clusters
 
-    @torch.no_grad()
+    @property
+    def downsample_factor(self):
+        # todo: double check
+        return 320
+
+    @torch.inference_mode()
     def forward(self, wav_input, flatten=True, input_sample_hz=None):
-        device = wav_input.device
+        batch, device = wav_input.shape[0], wav_input.device
 
         if exists(input_sample_hz):
             wav_input = resample(wav_input, input_sample_hz,
@@ -64,15 +86,26 @@ class HubertWithKmeans(nn.Module):
         if exists(self.seq_len_multiple_of):
             wav_input = curtail_to_multiple(wav_input, self.seq_len_multiple_of)
 
-        embed = self.model(wav_input, features_only=True)
-        embed, packed_shape = pack([embed['x']], '* d')
+        # 以下两种提取方式结果一样
+        # 但是我们使用 model.extract_features 和 exp/dump_hubert_feature.py 保持一致
+        # embed = self.model(
+        #     wav_input,
+        #     features_only=True,
+        #     mask=False,  # thanks to @maitycyrus for noticing that mask is defaulted to True in the fairseq code
+        #     output_layer=self.output_layer)['x']
 
-        codebook_indices = self.kmeans.predict(embed.cpu().detach().numpy())
+        embed, _ = self.model.extract_features(
+                    source=wav_input,
+                    padding_mask=None,
+                    mask=False,
+                    output_layer=self.output_layer, )
 
-        codebook_indices = torch.from_numpy(codebook_indices).to(device).long()
+        batched_cluster_centers = repeat(
+            self.cluster_centers, 'c d -> b c d', b=embed.shape[0])
+        dists = -torch.cdist(embed, batched_cluster_centers, p=2)
+        clusters = dists.argmax(dim=-1)
 
         if flatten:
-            return codebook_indices
+            return clusters
 
-        codebook_indices, = unpack(codebook_indices, packed_shape, '*')
-        return codebook_indices
+        return rearrange(clusters, 'b ... -> b (...)')
--- soundstorm/s2/models/hubert/semantic_tokenizer.py
+++ soundstorm/s2/models/hubert/semantic_tokenizer.py
@@ -4,17 +4,18 @@ from typing import Union
 
 import librosa
 import torch
+
 from soundstorm.s2.models.hubert.abs_tokenizer import AbsTokenizer
 from soundstorm.s2.models.hubert.hubert_kmeans import HubertWithKmeans
 
 
 class SemanticTokenizer(AbsTokenizer):
-    def __init__(
-            self,
-            hubert_path,
-            quantizer_path,
-            duplicate: bool=False,
-            device: Union[str, torch.device]=None, ):
+    def __init__(self,
+                 hubert_path,
+                 quantizer_path,
+                 duplicate: bool=False,
+                 device: Union[str, torch.device]=None,
+                 output_layer=9):
         """  Hubert model for extract semantic token
         """
         super(SemanticTokenizer, self).__init__()
@@ -27,7 +28,9 @@ class SemanticTokenizer(AbsTokenizer):
         self.hubert_path = hubert_path
         self.quantizer_path = quantizer_path
         self.hubert_kmeans = HubertWithKmeans(
-            checkpoint_path=self.hubert_path, kmeans_path=self.quantizer_path)
+            checkpoint_path=self.hubert_path,
+            kmeans_path=self.quantizer_path,
+            output_layer=output_layer)
         self.hubert_kmeans = self.hubert_kmeans.to(self.device)
 
         logging.info(f"hubert semantic model works on {self.device}")
@@ -97,16 +100,24 @@ class SemanticTokenizer(AbsTokenizer):
 if __name__ == '__main__':
     # mHuBERT sr = 16k, hop_size = 320
     # mHuBERT 和 HuBERT 都是 16k 320
-    hubert_path = './hubert_base_ls960.pt'
-    quantizer_path = './hubert_base_ls960_L9_km500.bin'
+    # base -> ok
+    # hubert_path = './hubert_base_ls960.pt'
+    # quantizer_path = './hubert_base_ls960_L9_km500.bin'
+
+    # large
+    hubert_path = './hubert_large_ll60k.pt'
+    quantizer_path = './libritts-360_hubert_large_ll60k_L12_km1024.bin'
     # [1, 142778]
     # LibriTTS-R 中的两条提取结果和 https://github.com/yangdongchao/SoundStorm/blob/master/data_sample/LibriTTS_1000/semantic/test.tsv
     # 中提供的在长度和数值上有些微的差别
-    wav = './LJ050-0278.wav'
+    wav = './LJ050-0278_16k.wav'
     wav, _ = librosa.load(wav, sr=16000)
     wav = torch.tensor(wav).unsqueeze(0)
     tokenizer = SemanticTokenizer(
-        hubert_path=hubert_path, quantizer_path=quantizer_path, duplicate=True)
+        hubert_path=hubert_path,
+        quantizer_path=quantizer_path,
+        duplicate=True,
+        output_layer=12)
     # wav should be wav_path or torch.Tensor
     flat_codec = tokenizer.tokenize(wav)
     print('flat_codec:', flat_codec)
@@ -115,3 +126,5 @@ if __name__ == '__main__':
     # 142778/320 = 446.18125
     # 需要处理不整除的情况
     print('flat_codec.shape:', flat_codec.shape)
+    import numpy as np
+    np.save("flat_codec_final.npy", flat_codec.cpu().numpy())
