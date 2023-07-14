85ded6a166e20be4e69d23c2790f34e2cfcab8ec
TianYuan <white-sky@qq.com>
2023-07-14 08:21:32 +0000

    fix input dataset

--- egs_s1/AR/LibriTTS/local/train.sh
+++ egs_s1/AR/LibriTTS/local/train.sh
@@ -7,8 +7,8 @@ dump_dir=$4
 
 python3 ${BIN_DIR}/train.py \
     --config_file=${config_path} \
-    --train_semantic_path=${root_dir}/${dump_dir}/test/semantic_token.tsv \
-    --train_phoneme_path=${root_dir}/${dump_dir}/test/phonemes.npy \
+    --train_semantic_path=${root_dir}/${dump_dir}/train/semantic_token.tsv \
+    --train_phoneme_path=${root_dir}/${dump_dir}/train/phonemes.npy \
     --dev_semantic_path=${root_dir}/${dump_dir}/dev/semantic_token.tsv \
     --dev_phoneme_path=${root_dir}/${dump_dir}/dev/phonemes.npy \
     --output_dir=${root_dir}/${train_output_path}
