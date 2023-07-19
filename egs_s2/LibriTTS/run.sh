13850f22dfa8c38733d8dbe7a0538ccaf2bfb78b
TianYuan <white-sky@qq.com>
2023-07-19 04:57:49 +0000

    set 30k_basex2_hubert_L9km500.yaml as default.yaml for LibriTTS S2

--- egs_s2/LibriTTS/run.sh
+++ egs_s2/LibriTTS/run.sh
@@ -18,7 +18,7 @@ dist_url='tcp://127.0.0.1:29501'
 # use which checkpoint file to test
 ckpt_name=last.pth
 # should be same with ${layer} in hubert_kms.sh
-layer=10
+layer=9
 # should be same with ${hubert_path} in hubert_kms.sh
 hubert_path=pretrained_model/hubert/hubert_base_ls960.pt
 quantizer_path=pretrained_model/hubert/hubert_base_ls960_L9_km500.bin
