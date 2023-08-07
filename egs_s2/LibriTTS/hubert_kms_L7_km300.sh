2997486b7359183585ccffccb4a92e0e9f9f8ad6
TianYuan <white-sky@qq.com>
2023-08-07 09:42:25 +0000

    add wait for hubert_kms

--- egs_s2/LibriTTS/hubert_kms_L7_km300.sh
+++ egs_s2/LibriTTS/hubert_kms_L7_km300.sh
@@ -32,6 +32,7 @@ fi
 # dump hubert feature
 # generate in parallel
 if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ]; then
+    echo "dump_hubert_feature.py start !"
     CUDA_VISIBLE_DEVICES=0 python3 ${BIN_DIR}/hubert/dump_hubert_feature.py \
         --tsv_dir=${root_dir}/${dump_dir}/${sub_dataset_name} \
         --split=audio_files \
@@ -39,112 +40,146 @@ if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ]; then
         --feat_dir=${root_dir}/${dump_dir}/${sub_dataset_name}/semantic_feature_L${layer} \
         --layer=${layer} \
         --nshard=16 \
-        --rank=0 & CUDA_VISIBLE_DEVICES=0 python3 ${BIN_DIR}/hubert/dump_hubert_feature.py \
+        --rank=0 &
+    pid0="$!"
+    CUDA_VISIBLE_DEVICES=0 python3 ${BIN_DIR}/hubert/dump_hubert_feature.py \
         --tsv_dir=${root_dir}/${dump_dir}/${sub_dataset_name} \
         --split=audio_files \
         --ckpt_path=${hubert_path} \
         --feat_dir=${root_dir}/${dump_dir}/${sub_dataset_name}/semantic_feature_L${layer} \
         --layer=${layer} \
         --nshard=16 \
-        --rank=1 & CUDA_VISIBLE_DEVICES=1 python3 ${BIN_DIR}/hubert/dump_hubert_feature.py \
+        --rank=1 &
+    pid1="$!"
+    CUDA_VISIBLE_DEVICES=1 python3 ${BIN_DIR}/hubert/dump_hubert_feature.py \
         --tsv_dir=${root_dir}/${dump_dir}/${sub_dataset_name} \
         --split=audio_files \
         --ckpt_path=${hubert_path} \
         --feat_dir=${root_dir}/${dump_dir}/${sub_dataset_name}/semantic_feature_L${layer} \
         --layer=${layer} \
         --nshard=16 \
-        --rank=2 & CUDA_VISIBLE_DEVICES=1 python3 ${BIN_DIR}/hubert/dump_hubert_feature.py \
+        --rank=2 &
+    pid2="$!"
+    CUDA_VISIBLE_DEVICES=1 python3 ${BIN_DIR}/hubert/dump_hubert_feature.py \
         --tsv_dir=${root_dir}/${dump_dir}/${sub_dataset_name} \
         --split=audio_files \
         --ckpt_path=${hubert_path} \
         --feat_dir=${root_dir}/${dump_dir}/${sub_dataset_name}/semantic_feature_L${layer} \
         --layer=${layer} \
         --nshard=16 \
-        --rank=3 & CUDA_VISIBLE_DEVICES=2 python3 ${BIN_DIR}/hubert/dump_hubert_feature.py \
+        --rank=3 &
+    pid3="$!"
+    CUDA_VISIBLE_DEVICES=2 python3 ${BIN_DIR}/hubert/dump_hubert_feature.py \
         --tsv_dir=${root_dir}/${dump_dir}/${sub_dataset_name} \
         --split=audio_files \
         --ckpt_path=${hubert_path} \
         --feat_dir=${root_dir}/${dump_dir}/${sub_dataset_name}/semantic_feature_L${layer} \
         --layer=${layer} \
         --nshard=16 \
-        --rank=4 & CUDA_VISIBLE_DEVICES=2 python3 ${BIN_DIR}/hubert/dump_hubert_feature.py \
+        --rank=4 &
+    pid4="$!"
+    CUDA_VISIBLE_DEVICES=2 python3 ${BIN_DIR}/hubert/dump_hubert_feature.py \
         --tsv_dir=${root_dir}/${dump_dir}/${sub_dataset_name} \
         --split=audio_files \
         --ckpt_path=${hubert_path} \
         --feat_dir=${root_dir}/${dump_dir}/${sub_dataset_name}/semantic_feature_L${layer} \
         --layer=${layer} \
         --nshard=16 \
-        --rank=5 & CUDA_VISIBLE_DEVICES=3 python3 ${BIN_DIR}/hubert/dump_hubert_feature.py \
+        --rank=5 &
+    pid5="$!"
+    CUDA_VISIBLE_DEVICES=3 python3 ${BIN_DIR}/hubert/dump_hubert_feature.py \
         --tsv_dir=${root_dir}/${dump_dir}/${sub_dataset_name} \
         --split=audio_files \
         --ckpt_path=${hubert_path} \
         --feat_dir=${root_dir}/${dump_dir}/${sub_dataset_name}/semantic_feature_L${layer} \
         --layer=${layer} \
         --nshard=16 \
-        --rank=6 & CUDA_VISIBLE_DEVICES=3 python3 ${BIN_DIR}/hubert/dump_hubert_feature.py \
+        --rank=6 &
+    pid6="$!"
+    CUDA_VISIBLE_DEVICES=3 python3 ${BIN_DIR}/hubert/dump_hubert_feature.py \
         --tsv_dir=${root_dir}/${dump_dir}/${sub_dataset_name} \
         --split=audio_files \
         --ckpt_path=${hubert_path} \
         --feat_dir=${root_dir}/${dump_dir}/${sub_dataset_name}/semantic_feature_L${layer} \
         --layer=${layer} \
         --nshard=16 \
-        --rank=7 & CUDA_VISIBLE_DEVICES=4 python3 ${BIN_DIR}/hubert/dump_hubert_feature.py \
+        --rank=7 &
+    pid7="$!"
+    CUDA_VISIBLE_DEVICES=4 python3 ${BIN_DIR}/hubert/dump_hubert_feature.py \
         --tsv_dir=${root_dir}/${dump_dir}/${sub_dataset_name} \
         --split=audio_files \
         --ckpt_path=${hubert_path} \
         --feat_dir=${root_dir}/${dump_dir}/${sub_dataset_name}/semantic_feature_L${layer} \
         --layer=${layer} \
         --nshard=16 \
-        --rank=8 & CUDA_VISIBLE_DEVICES=4 python3 ${BIN_DIR}/hubert/dump_hubert_feature.py \
+        --rank=8 &
+    pid8="$!"
+    CUDA_VISIBLE_DEVICES=4 python3 ${BIN_DIR}/hubert/dump_hubert_feature.py \
         --tsv_dir=${root_dir}/${dump_dir}/${sub_dataset_name} \
         --split=audio_files \
         --ckpt_path=${hubert_path} \
         --feat_dir=${root_dir}/${dump_dir}/${sub_dataset_name}/semantic_feature_L${layer} \
         --layer=${layer} \
         --nshard=16 \
-        --rank=9 & CUDA_VISIBLE_DEVICES=5 python3 ${BIN_DIR}/hubert/dump_hubert_feature.py \
+        --rank=9 &
+    pid9="$!"
+    CUDA_VISIBLE_DEVICES=5 python3 ${BIN_DIR}/hubert/dump_hubert_feature.py \
         --tsv_dir=${root_dir}/${dump_dir}/${sub_dataset_name} \
         --split=audio_files \
         --ckpt_path=${hubert_path} \
         --feat_dir=${root_dir}/${dump_dir}/${sub_dataset_name}/semantic_feature_L${layer} \
         --layer=${layer} \
         --nshard=16 \
-        --rank=10 & CUDA_VISIBLE_DEVICES=5 python3 ${BIN_DIR}/hubert/dump_hubert_feature.py \
+        --rank=10 &
+    pid10="$!"
+    CUDA_VISIBLE_DEVICES=5 python3 ${BIN_DIR}/hubert/dump_hubert_feature.py \
         --tsv_dir=${root_dir}/${dump_dir}/${sub_dataset_name} \
         --split=audio_files \
         --ckpt_path=${hubert_path} \
         --feat_dir=${root_dir}/${dump_dir}/${sub_dataset_name}/semantic_feature_L${layer} \
         --layer=${layer} \
         --nshard=16 \
-        --rank=11 & CUDA_VISIBLE_DEVICES=6 python3 ${BIN_DIR}/hubert/dump_hubert_feature.py \
+        --rank=11 &
+    pid11="$!"
+    CUDA_VISIBLE_DEVICES=6 python3 ${BIN_DIR}/hubert/dump_hubert_feature.py \
         --tsv_dir=${root_dir}/${dump_dir}/${sub_dataset_name} \
         --split=audio_files \
         --ckpt_path=${hubert_path} \
         --feat_dir=${root_dir}/${dump_dir}/${sub_dataset_name}/semantic_feature_L${layer} \
         --layer=${layer} \
         --nshard=16 \
-        --rank=12 & CUDA_VISIBLE_DEVICES=6 python3 ${BIN_DIR}/hubert/dump_hubert_feature.py \
+        --rank=12 &
+    pid12="$!"
+    CUDA_VISIBLE_DEVICES=6 python3 ${BIN_DIR}/hubert/dump_hubert_feature.py \
         --tsv_dir=${root_dir}/${dump_dir}/${sub_dataset_name} \
         --split=audio_files \
         --ckpt_path=${hubert_path} \
         --feat_dir=${root_dir}/${dump_dir}/${sub_dataset_name}/semantic_feature_L${layer} \
         --layer=${layer} \
         --nshard=16 \
-        --rank=13 & CUDA_VISIBLE_DEVICES=7 python3 ${BIN_DIR}/hubert/dump_hubert_feature.py \
+        --rank=13 &
+    pid13="$!"
+    CUDA_VISIBLE_DEVICES=7 python3 ${BIN_DIR}/hubert/dump_hubert_feature.py \
         --tsv_dir=${root_dir}/${dump_dir}/${sub_dataset_name} \
         --split=audio_files \
         --ckpt_path=${hubert_path} \
         --feat_dir=${root_dir}/${dump_dir}/${sub_dataset_name}/semantic_feature_L${layer} \
         --layer=${layer} \
         --nshard=16 \
-        --rank=14 & CUDA_VISIBLE_DEVICES=7 python3 ${BIN_DIR}/hubert/dump_hubert_feature.py \
+        --rank=14 &
+    pid14="$!"
+    CUDA_VISIBLE_DEVICES=7 python3 ${BIN_DIR}/hubert/dump_hubert_feature.py \
         --tsv_dir=${root_dir}/${dump_dir}/${sub_dataset_name} \
         --split=audio_files \
         --ckpt_path=${hubert_path} \
         --feat_dir=${root_dir}/${dump_dir}/${sub_dataset_name}/semantic_feature_L${layer} \
         --layer=${layer} \
         --nshard=16 \
-        --rank=15
+        --rank=15 &
+    pid15="$!"
+    wait "$pid0" "$pid1" "$pid2" "$pid3" "$pid4" "$pid5" "$pid6" "$pid7" \
+    "$pid8" "$pid9" "$pid10" "$pid11" "$pid12" "$pid13" "$pid14" "$pid15"
+    echo "dump_hubert_feature.py done !"
 fi
 # learn kmeans
 if [ ${stage} -le 2 ] && [ ${stop_stage} -ge 2 ]; then
