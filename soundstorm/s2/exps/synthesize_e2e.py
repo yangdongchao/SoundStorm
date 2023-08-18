61ec7629067cd2e43ec816953bb04d17e7cbde4d
TianYuan <white-sky@qq.com>
2023-08-18 08:19:57 +0000

    add RTF for S2

--- soundstorm/s2/exps/synthesize_e2e.py
+++ soundstorm/s2/exps/synthesize_e2e.py
@@ -2,7 +2,6 @@
 import argparse
 import os
 import re
-import time
 from pathlib import Path
 
 import librosa
@@ -16,6 +15,7 @@ from soundstorm.s1.AR.text_processing.phonemizer import GruutPhonemizer
 from soundstorm.s2.models.dalle_wav.build import build_model
 from soundstorm.s2.models.hubert.semantic_tokenizer import SemanticTokenizer
 from soundstorm.utils.io import load_yaml_config
+from timer import timer
 
 acoustic_token_nums = 1024
 
@@ -54,7 +54,7 @@ def hificodec_decode(hificodec, acoustic_token, rescale=True):
 def get_S1_batch(text, phonemizer):
     # phoneme_ids 和 phoneme_ids_len 是需要的
     phoneme = phonemizer.phonemize(text, espeak=False)
-    print("phoneme:", phoneme)
+    # print("phoneme:", phoneme)
     phoneme = " " + phoneme
     phoneme_ids = phonemizer.transform(phoneme)
     phoneme_ids_len = len(phoneme_ids)
@@ -62,7 +62,7 @@ def get_S1_batch(text, phonemizer):
     # add batch axis here
     phoneme_ids = torch.tensor(phoneme_ids).unsqueeze(0)
     phoneme_ids_len = torch.tensor([phoneme_ids_len])
-    print("phoneme_ids:", phoneme_ids)
+    # print("phoneme_ids:", phoneme_ids)
     batch = {
         # torch.Tensor (B, max_phoneme_length) 
         "phoneme_ids": phoneme_ids,
@@ -75,19 +75,19 @@ def get_S1_batch(text, phonemizer):
 def get_S1_prompt(wav, asr_model, phonemizer, semantic_tokenizer, sil_token):
     # wav 需要挪出末尾的静音否则也可能提前停住
     prompt_text = asr_model.transcribe(wav)["text"]
-    print("prompt_text:", prompt_text)
+    # print("prompt_text:", prompt_text)
     # 移除最后的句点, 防止 AR S1 infer 提前停止, 加了句点可能会有停顿或者噪音
     # prompt_text = prompt_text.replace(".", "")
     prompt_phoneme = phonemizer.phonemize(prompt_text, espeak=False)
     # 移除 prompt_phoneme 末尾的 . 但是保留空格, 因为直接移除 prompt_text 会导致 prompt_phoneme 末尾少一个空格
     prompt_phoneme = prompt_phoneme.rstrip(".")
-    print("prompt_phoneme:", prompt_phoneme)
+    # print("prompt_phoneme:", prompt_phoneme)
     # 末尾不是空格的则加一个空格
     if not prompt_phoneme.endswith(' '):
         prompt_phoneme = prompt_phoneme + ' '
     # 16 表示空格
     prompt_phoneme_ids = phonemizer.transform(prompt_phoneme)
-    print("prompt_phoneme_ids:", prompt_phoneme_ids)
+    # print("prompt_phoneme_ids:", prompt_phoneme_ids)
     prompt_phoneme_ids_len = len(prompt_phoneme_ids)
     # get prompt_semantic
     # (T) -> (1, T)
@@ -95,11 +95,11 @@ def get_S1_prompt(wav, asr_model, phonemizer, semantic_tokenizer, sil_token):
     wav = wav.cuda()
     # (1, T)
     prompt_semantic_tokens = semantic_tokenizer.tokenize(wav).to(torch.int32)
-    print("prompt_semantic_tokens:", prompt_semantic_tokens)
+    # print("prompt_semantic_tokens:", prompt_semantic_tokens)
     # remove sil token in the end
     while prompt_semantic_tokens[0][-1] == sil_token:
         prompt_semantic_tokens = prompt_semantic_tokens[..., :-1]
-        print("prompt_semantic_tokens:", prompt_semantic_tokens)
+        # print("prompt_semantic_tokens:", prompt_semantic_tokens)
     prompt_phoneme_ids = torch.tensor(prompt_phoneme_ids).unsqueeze(0)
     prompt_phoneme_ids_len = torch.tensor([prompt_phoneme_ids_len])
 
@@ -174,6 +174,7 @@ def get_S2_batch(prompt_semantic_tokens,
 
 
 def get_prompt_wav(prompt_wav_path, sample_rate):
+    print("----------------------------------------")
     prompt_wav, _ = librosa.load(prompt_wav_path, sr=sample_rate)
     # 取末尾 3s, 但是不包含最后 0.1s 防止 AR S1 infer 提前停止
     print("prompt_wav.shape:", prompt_wav.shape)
@@ -194,6 +195,7 @@ def get_prompt_wav(prompt_wav_path, sample_rate):
     # np_zero = np.array([0] * int(sample_rate / 200), dtype='float32')
     # prompt_wav = np.concatenate([prompt_wav, np_zero])
     print("prompt_wav.shape after add sil:", prompt_wav.shape)
+    print("----------------------------------------")
     return prompt_wav
 
 
@@ -252,57 +254,73 @@ def evaluate(args,
     prompt_semantic_tokens = S2_prompt_result['prompt_semantic_tokens']
     prompt_acoustic_tokens = S2_prompt_result['prompt_acoustic_tokens']
 
+    N = 0
+    T = 0
+
     # 遍历 utt_id
     for utt_id, sentence in sentences:
-        S1_batch = get_S1_batch(sentence, phonemizer)
-        # prompt 和真正的输入拼接
-        S1_all_phoneme_ids = torch.cat(
-            [S1_prompt_phoneme_ids, S1_batch['phoneme_ids']], dim=1)
-        S1_all_phoneme_len = S1_prompt_phoneme_ids_len + S1_batch[
-            'phoneme_ids_len']
-        S1_st = time.time()
-        with torch.no_grad():
-            S1_pred_semantic = S1_model.model.infer(
-                S1_all_phoneme_ids.cuda(),
-                S1_all_phoneme_len.cuda(),
-                S1_prompt.cuda(),
-                top_k=S1_top_k,
-                early_stop_num=hz * S1_max_sec)
-        print(f'{time.time() - S1_st} sec used in T2S')
+        with timer() as t:
+            S1_batch = get_S1_batch(sentence, phonemizer)
+            # prompt 和真正的输入拼接
+            S1_all_phoneme_ids = torch.cat(
+                [S1_prompt_phoneme_ids, S1_batch['phoneme_ids']], dim=1)
+            S1_all_phoneme_len = S1_prompt_phoneme_ids_len + S1_batch[
+                'phoneme_ids_len']
+            S1_st = t.elapse
+            with torch.no_grad():
+                S1_pred_semantic = S1_model.model.infer(
+                    S1_all_phoneme_ids.cuda(),
+                    S1_all_phoneme_len.cuda(),
+                    S1_prompt.cuda(),
+                    top_k=S1_top_k,
+                    early_stop_num=hz * S1_max_sec)
+            S1_end = t.elapse
 
-        # 删除 prompt 对应的部分
-        S1_prompt_len = S1_prompt.shape[-1]
-        print("S1_prompt_len:", S1_prompt_len)
-        # (1, T)
-        # target phone 开头有多余的 " ", 所以这里可以多截掉大约 3 个
-        S1_pred_semantic = S1_pred_semantic[:, S1_prompt_len + 3:]
+            # 删除 prompt 对应的部分
+            S1_prompt_len = S1_prompt.shape[-1]
+            # print("S1_prompt_len:", S1_prompt_len)
+            # (1, T)
+            # target phone 开头有多余的 " ", 所以这里可以多截掉大约 3 个
+            S1_pred_semantic = S1_pred_semantic[:, S1_prompt_len + 3:]
 
-        # get target semnatic from prompt wav and text
-        S2_batch = get_S2_batch(
-            prompt_semantic_tokens=prompt_semantic_tokens,
-            prompt_acoustic_tokens=prompt_acoustic_tokens,
-            target_semantic_tokens=S1_pred_semantic,
-            num_quant=num_quant,
-            hz=hz,
-            max_prompt_sec=S2_max_prompt_sec,
-            max_target_sec=S2_max_target_sec)
+            # get target semnatic from prompt wav and text
+            S2_batch = get_S2_batch(
+                prompt_semantic_tokens=prompt_semantic_tokens,
+                prompt_acoustic_tokens=prompt_acoustic_tokens,
+                target_semantic_tokens=S1_pred_semantic,
+                num_quant=num_quant,
+                hz=hz,
+                max_prompt_sec=S2_max_prompt_sec,
+                max_target_sec=S2_max_target_sec)
 
-        S2_batch = move_tensors_to_cuda(S2_batch)
+            S2_batch = move_tensors_to_cuda(S2_batch)
 
-        S2_st = time.time()
-        with torch.no_grad():
-            model_out = S2_model.infer_one(S2_batch, temperature=temperature)
-        print(f'{time.time() - S2_st} sec used in S2A')
+            S2_st = t.elapse
+            with torch.no_grad():
+                model_out = S2_model.infer_one(
+                    S2_batch, temperature=temperature)
+            S2_end = t.elapse
 
-        content = model_out['token_pred']
-        # shape (B, Nq x T) -> (B, Nq, T)
-        codes = content.reshape(content.shape[0], num_quant, -1)
-        wav_gen = hificodec_decode(hificodec, codes)
-        print("S1_top_k:", S1_top_k)
+            content = model_out['token_pred']
+            # shape (B, Nq x T) -> (B, Nq, T)
+            codes = content.reshape(content.shape[0], num_quant, -1)
+            wav_gen = hificodec_decode(hificodec, codes)
+
+        N += wav_gen.size
+        T += t.elapse
+        speed = wav_gen.size / t.elapse
+        rtf = sample_rate / speed
+        print(
+            f"{utt_id},  wave: {wav_gen.shape}, time: {t.elapse}s, Hz: {speed}, RTF: {rtf}."
+        )
+        print(f'{S1_end - S1_st} sec used in T2S')
+        print(f'{S2_end - S2_st} sec used in S2A')
+
         sf.write(
             output_dir /
             f"S1_topk_{S1_top_k}_temperature_{temperature}_s_{prompt_name}_t_{utt_id}.wav",
             np.concatenate([prompt_wav, wav_gen]), sample_rate)
+    print(f"generation speed: {N / T}Hz, RTF: {sample_rate / (N / T) }")
 
 
 def parse_args():
